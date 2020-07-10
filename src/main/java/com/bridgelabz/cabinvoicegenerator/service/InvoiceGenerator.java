af02dea1e90a2cc9abb6fca1da3ed96ca309285a
“Shubham” <shubhampattar2@gmail.com>
2020-07-10 19:07:11 +0530

    ADDED Code to calculate total fare for given date and time.

--- src/main/java/com/bridgelabz/cabinvoicegenerator/service/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/service/InvoiceGenerator.java
@@ -2,9 +2,11 @@ package com.bridgelabz.cabinvoicegenerator.service;
 
 public class InvoiceGenerator
 {
+    private static final double COST_PER_KM = 10.0;
+    private static final int COST_PER_MIN = 1;
 
     public double calculateFare(double distance, int time)
     {
-        return 0;
+        return distance * COST_PER_KM + time * COST_PER_MIN;
     }
 }
