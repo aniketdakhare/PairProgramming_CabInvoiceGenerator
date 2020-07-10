2ed4bb6610b8f1cc46946c5695f2549dd21f77d4
“Aniket_D” <aniketdakhare@gmail.com>
2020-07-10 18:54:54 +0530

    ADDED Test Case for calculating total fare after providing distance and time.

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/service/InvoiceGenerator.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.cabinvoicegenerator.service;
+
+public class InvoiceGenerator
+{
+
+    public double calculateFare(double distance, int time)
+    {
+        return 0;
+    }
+}
